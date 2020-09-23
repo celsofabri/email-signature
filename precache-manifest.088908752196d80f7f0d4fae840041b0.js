6e09552a815d30d689ce0dbbc36799c9cb40f772
Celso Fabri Junior <celso.fabri@live.com>
2020-09-23 16:12:13 -0300

    Updates

---
+++ precache-manifest.088908752196d80f7f0d4fae840041b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f83192ea173be9ee28fe641a48aabb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c2069980c039d33a314",
+    "url": "./static/css/main.d2f82154.chunk.css"
+  },
+  {
+    "revision": "0b1d3955d60f54414e65",
+    "url": "./static/js/2.f25c150d.chunk.js"
+  },
+  {
+    "revision": "2428e7b5dc6863a61c8b2621ca23b500",
+    "url": "./static/js/2.f25c150d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2069980c039d33a314",
+    "url": "./static/js/main.f1ced0b7.chunk.js"
+  },
+  {
+    "revision": "624395534e82e4bac28e",
+    "url": "./static/js/runtime-main.5fcddd95.js"
+  },
+  {
+    "revision": "dd2fe3e0a53cfd0b89f48ccc6c7aa5d1",
+    "url": "./static/media/ateliware-icon.dd2fe3e0.svg"
+  },
+  {
+    "revision": "d252329370a2a40a4783d24269a79155",
+    "url": "./static/media/ateliware-logo.d2523293.svg"
+  }
+]);
